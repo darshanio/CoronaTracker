bff04ea90998c5b22989683f241378bab39d5e07
Darshan Panchal <darshan.m.panchal@outlook.com>
2020-04-04 20:30:21 +0530

    Added build configurations;

--- CoronaTracker/CoronaTracker.Android/MainActivity.cs
+++ CoronaTracker/CoronaTracker.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace CoronaTracker.Droid
 {
-    [Activity(Label = "CoronaTracker", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
